68b7cd3f73b4473003e49421051848af47636558
Amr Bassal <amrbassal98@gmail.com>
2026-07-14 17:38:08 +0300

    feat(admin): register Document model with tailored admin interface

--- analytics_engine/admin.py
+++ analytics_engine/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("title", "file_type", "uploaded_at")
+    list_filter = ("file_type", "uploaded_at")
+    search_fields = ("title", "raw_text")
+    ordering = ("-uploaded_at",)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("title", "file_type", "raw_text"),
+            },
+        ),
+        (
+            "Analysis",
+            {
+                "classes": ("collapse",),
+                "fields": ("analysis_metadata", "polymorphic_payload"),
+            },
+        ),
+        (
+            "Annotations",
+            {
+                "classes": ("collapse",),
+                "fields": ("annotations",),
+            },
+        ),
+    )
+
+    readonly_fields = ("uploaded_at",)
